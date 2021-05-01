3d7a22e9f9798afd28a52e126001fcc3cb84ad6f
Brandon <lordizuriel@gmail.com>
2021-05-01 13:29:08 -0500

    documentation yay

--- src/Atom.ts
+++ src/Atom.ts
@@ -4,7 +4,7 @@ import BaseAtom from './BaseAtom';
  * A function that will update the value of an atom. It receives the atom's
  * current value as its argument and is expected to return the atom's next value
  * as the return value.
- * @typearg T The type of the atom's value.
+ * @typeparam T The type of the atom's value.
  */
 export interface AtomUpdateFunction<T> {
 	(arg: T): T;
@@ -13,19 +13,67 @@ export interface AtomUpdateFunction<T> {
 /**
  * An atom updater is either a new value or a function that will generate the
  * next value for the atom.
- * @typearg T The type of the atom's value.
+ * @typeparam T The type of the atom's value.
  */
 export type AtomUpdater<T> = T | AtomUpdateFunction<T>;
 
+/**
+ * Atom is the most basic unit of state. It contains a single value that can be
+ * set or updated in many places and these changes can be subscribed to allowing
+ * your application to be reactive. There are two ways to update an atoms value,
+ * you can either set it or udpate it. Setting the atom's value will clear all
+ * future updates and replace the value immediately, notifying subscribers. If
+ * you opt to update the value instead then your update function (or value) will
+ * be applied after all previous updates have been applied. This is useful if
+ * each new state value builds on the previous state value and ensures that
+ * each update is firing with the latest value as it's argument.
+ *
+ * ### Example
+ *
+ * ```typescript
+ * const counter = atom(0);
+ *
+ * const countDisplay = document.querySelector('#count-display');
+ * counter.subscribe(atom => {
+ *   countDisplay.textContent = `Current count: ${atom.value}`;
+ * });
+ *
+ * const incrementButton = document.querySelector('#increment-button');
+ * incrementButton.addEventListener('click', () => counter.update(n => n + 1));
+ * ```
+ *
+ * @typeparam T The type of the atom's value.
+ */
 export default class Atom<T> extends BaseAtom<T> {
+	/**
+	 * The updater queue used to keep track of the order and amount of requested
+	 * updates to the atom's current value.
+	 */
 	#updaterQueue: AtomUpdateFunction<T>[];
+
+	/**
+	 * The update timeout, this is tracked since setting an atom's value will
+	 * clear future updates so must cancel the asynchronous update timer.
+	 */
 	#updateTimeout: ReturnType<typeof setTimeout> | undefined;
 
+	/**
+	 * Creates a new Atom with an initial value.
+	 * @param value The intitial value of the atom.
+	 */
 	constructor(value: T) {
 		super(value);
 		this.#updaterQueue = [];
 	}
 
+	/**
+	 * Queue and update for the atom's value. There is no guarantee that when this
+	 * is called the value will be udpdated immediately, the update is queued and
+	 * set up to be called in order with other updates. After each update the atom
+	 * will notify subscribers the value has changed.
+	 * @param updater The new value, or update function, that will be called when
+	 *   this update's place in the queue is reached.
+	 */
 	update(updater: AtomUpdater<T>): void {
 		let newUpdater: AtomUpdateFunction<T>;
 		if (typeof updater !== 'function') {
@@ -37,12 +85,26 @@ export default class Atom<T> extends BaseAtom<T> {
 		this.queueUpdate(newUpdater);
 	}
 
+	/**
+	 * Set, much like update, will modify the value of the atom. The core difference
+	 * between set and update though is that set is immediate and will clear any
+	 * future updates. If you think of the counter example you would prefer to
+	 * use update to add or subtract from the current value; but, you would want
+	 * to use set in the case of resetting it's value back to 0 as it would be
+	 * immediate and also cancel any pending updates. Set does still notify
+	 * subscribers that the value has changed.
+	 * @param value The new value of the atom that should be set immediately.
+	 */
 	set(value: T): void {
 		this.clearUpdateQueue();
 		this.setValue(value);
 		this.notifySubscribers();
 	}
 
+	/**
+	 * Clears the update queue, emptying it out as well as clearing an update
+	 * timeout if once is currently pending execution.
+	 */
 	protected clearUpdateQueue(): void {
 		if (this.#updateTimeout) {
 			clearTimeout(this.#updateTimeout);
@@ -51,6 +113,15 @@ export default class Atom<T> extends BaseAtom<T> {
 		this.#updaterQueue = [];
 	}
 
+	/**
+	 * This method is a helper for `update` and is responsible for actually
+	 * putting the update function into the queue. It also determines if needs to
+	 * initiate an update timer. The update timer is only active when there are
+	 * pending updates so it's possible we need to kick off the update timer in
+	 * cases where we're updating the value after a period of no updates.
+	 * @param updater The update function that should be added to the pending
+	 *   update queue.
+	 */
 	protected queueUpdate(updater: AtomUpdateFunction<T>): void {
 		this.#updaterQueue.push(updater);
 
@@ -59,6 +130,14 @@ export default class Atom<T> extends BaseAtom<T> {
 		}
 	}
 
+	/**
+	 * This method is a helper and is what is forward to setTimeout to handle
+	 * dequeing the latest update and executing it. This method will also clear
+	 * the update timer so that it's always unset when there is no timer running.
+	 * Once the update is applied it will then reset the timeout if there are
+	 * still updates remaining in the queue otherwise it ends without restarting
+	 * a timer.
+	 */
 	protected runUpdate = (): void => {
 		this.#updateTimeout = undefined;
 
--- src/atoms.ts
+++ src/atoms.ts
@@ -3,7 +3,57 @@ import Atom from './Atom';
 
 /**
  * readonlyAtom will create a new ReadonlyAtom instance with the provided
- * initial value and start function.
+ * initial value and start function. Readonly atoms are atoms that do not allow
+ * users to set or modify values, just read; however, this does not mean their
+ * value has to be static. If only an initialValue is provided then the returned
+ * readonly atom will have a static value, however if you also pass in a start
+ * function, this will receive a set function as an argument and from here you
+ * can do whatever you might need, such as running a timeout or interval or
+ * any other mechanism you need to periodically update the atoms value.
+ *
+ * ### Examples
+ *
+ * A really common example case is a clock:
+ *
+ * ```typescript
+ * const clock = readonlyAtom(new Date(), set => {
+ *   set(new Date());
+ *
+ *   const interval = setInterval(() => {
+ *     set(new Date());
+ *   }, 1000);
+ *
+ *   return () => {
+ *     clearInterval(interval);
+ *   };
+ * });
+ * ```
+ *
+ * See it on [CodePen](https://codepen.io/bbuck/pen/dyNBQev).
+ *
+ * Another possible example would be to handle other values set by something
+ * else, such as the window scroll event, which can be debounced to prevent
+ * unnecessary spam.
+ *
+ * ```typescript
+ * const scrollTop = readonlyAtom(scrollContainer.scrollTop, (set) => {
+ *   set(scrollContainer.scrollTop);
+ *
+ *   const onScroll = throttle(
+ *     () => {
+ *       set(scrollContainer.scrollTop);
+ *     }, 200, true);
+ *
+ *   scrollContainer.addEventListener("scroll", onScroll);
+ *
+ *   () => {
+ *     scrollContainer.removeEventListener("scroll", onScroll);
+ *   };
+ * });
+ * ```
+ *
+ * See it on [CodePen](https://codepen.io/bbuck/pen/wvgLQQY).
+ *
  * @param initialValue The initial value of the ReadonlyAtom.
  * @param start The start function that should run if the ReadonlyAtom is live
  *   or has subscribers.
@@ -13,4 +63,33 @@ import Atom from './Atom';
 export const readonlyAtom = <T>(initialValue: T, start?: ReadonlyAtomStarter<T>): ReadonlyAtom<T> =>
 	new ReadonlyAtom(initialValue, start);
 
+/**
+ * The atom is the core of @nuka/sate. It represents a piece of data that can
+ * be set or udpated anywhere in an application and provides the capability
+ * for a user to subscribe and listen for the changes. This allows your data to
+ * be used in a variety of places around your application while also allowing
+ * the consumers that need to respond to changes the capability of knowing when
+ * the value has been modified. Examples can range from user authentication
+ * tokens, application objects, counts, or any other data where you may need
+ * access to it in more than one place but wish to be notified when it's value
+ * has changed.
+ *
+ * ### Example
+ *
+ * ```typescript
+ * const counter = atom(0);
+ *
+ * const countDisplay = document.querySelector('#count-display');
+ * counter.subscribe(atom => {
+ *   countDisplay.textContent = `Current count: ${atom.value}`;
+ * });
+ *
+ * const incrementButton = document.querySelector('#increment-button');
+ * incrementButton.addEventListener('click', () => counter.update(n => n + 1));
+ * ```
+ *
+ * @param value The initial value of the atom.
+ * @returns A new atom containing the initial value ready for use.
+ * @typeparam T The type of the atom's value.
+ */
 export const atom = <T>(value: T): Atom<T> => new Atom(value);

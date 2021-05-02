cbc447a16069991df3f4c2c0cae23d6dcd7c5ef7
Brandon <lordizuriel@gmail.com>
2021-05-01 21:45:57 -0500

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import BaseAtom from './BaseAtom';
 import ReadonlyAtom from './ReadonlyAtom';
 import Projector from './Projector';
+import Atom from './Atom';
 
 export * from './types';
 export * from './Atom';
 export * from './ReadonlyAtom';
+export * from './Projector';
 export * from './atoms';
 
-export { BaseAtom, ReadonlyAtom, Projector };
+export { BaseAtom, Atom, ReadonlyAtom, Projector };
